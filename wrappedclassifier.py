2929ab81060aef9f3a1788ed54f6371703108773
k.i.smith <k.i.smith@ee8fd04b-393d-0410-b01a-eb7fda750bab>
2007-10-30 13:08:04 +0000

    Probably just awaiting perturbation functions now to exhibit basic functionality.

--- wrappedclassifier.py
+++ wrappedclassifier.py
@@ -27,3 +27,27 @@ class WrappedClassifier(object):
         """
         self.classifier = classifier
         self.data = data
+        
+    def perturb(self):
+        """ Perturbs the contained classifier, evaluates it and returns a
+            new WrappedClassifier containing it.
+        """
+        newWrapper = WrappedClassifier(self.classifier.perturb(), self.data)
+        newWrapper._evaluate()
+        return newWrapper
+        
+    def _evaluate(self):
+        """ Evaluates the performance of the wrapped classifier.
+        """
+        results = self.classifier.classify(self.data)
+        self.confusion = [[0,0][0,0]]
+        for i in range(0,len(results)):
+            self.confusion[self.trainingOutputs[i]][results[i]] += 1
+        self.tpr = confusion[1][1]/sum(confusion[1])
+        self.fpr = confusion[0][0]/sum(confusion[0])
+        
+    def dominates(self, other):
+        """ Test for domination of other.
+            Returns True where tpr/fpr are equivalent.
+        """
+        return self.tpr >= other.tpr and self.fpr <= other.fpr
